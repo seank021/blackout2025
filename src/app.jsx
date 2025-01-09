8d8d995c55729234af9379f49de6379d32985e4a
Sean Kim <seahnnnnnn@gmail.com>
2025-01-10 00:30:07 +0900

    feat: 레포 생성, tailwindcss 적용

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-3xl font-bold h-[100vh] flex items-center justify-center">
+      Hello, BlackOut!
+    </div>
+  );
+}
+
+export default App;
