fabac69f805d61eb67ab1c91bc2285d73e790fdf
Sean Kim <seahnnnnnn@gmail.com>
2025-01-12 05:35:20 +0900

    feat: store 마무리

--- src/components/store.jsx
+++ src/components/store.jsx
@@ -3,7 +3,7 @@ import SearchForm from './SearchForm';
 import StoreItem from './storeItem';
 import { useNavigate } from 'react-router-dom';
 import backBtn from '../assets/icons/backBtn.svg';
-import { getMyStore } from '../apis/api';
+import { getStore } from '../apis/api';
 
 const titleMap = {
     "마이": "me",
@@ -18,7 +18,7 @@ function Store({ title }) {
 
     useEffect(() => {
         const fetchStore = async () => {
-            const response = await getMyStore({ title: titleMap[title] });
+            const response = await getStore({ title: titleMap[title] });
             setStore(response.data);
         };
         fetchStore();
@@ -28,9 +28,28 @@ function Store({ title }) {
         if (store) {
             let total = 0;
             store.forEach((item) => {
-                item.my_prepay.forEach((prepay) => {
-                    total += prepay.credit;
-                });
+                if (title === '마이') {
+                    if (item.my_prepay) {
+                        item.my_prepay.forEach((prepay) => {
+                            total += prepay.credit;
+                        });
+                    }
+                } else if (title === '퍼블릭') {
+                    if (item.public_prepay) {
+                        console.log(item.public_prepay);
+                        item.public_prepay.forEach((prepay) => {
+                            total += prepay.credit;
+                        });
+                    }
+                } else if (title === '프라이빗') {
+                    if (item.private_prepay) {
+                        item.private_prepay.forEach((prepay) => {
+                            total += prepay.credit;
+                        });
+                    }
+                } else {
+                    return;
+                }
             });
             setTotalAmount(total);
         }
@@ -48,7 +67,7 @@ function Store({ title }) {
             {store && (
                 <div className="flex flex-col gap-[20px] mt-[20px] justify-center items-center">
                     {store.map((item) => (
-                        <StoreItem key={item.id} store={item} totalAmount={totalAmount} />
+                        <StoreItem key={item.id} store={item} totalAmount={totalAmount} title={title} />
                     ))}
                 </div>
             )}
