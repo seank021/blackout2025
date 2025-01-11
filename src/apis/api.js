b055922f6d6dc91f1767b602a2ef81d387f3b721
Sean Kim <seahnnnnnn@gmail.com>
2025-01-12 06:13:20 +0900

    feat: code confirm

--- src/apis/api.js
+++ src/apis/api.js
@@ -32,10 +32,30 @@ export const logIn = async ({ username, password }) => {
 // get store
 export const getStore = async ({ title }) => {
     const accessToken = localStorage.getItem("accessToken");
-    console.log(accessToken);
 
     try {
         const response = await axios.get(`${API_URL}/stores/${title}/`, {
+            headers: {
+                Authorization: `Token ${accessToken}`,
+            },
+        });
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.error(error);
+        return error.response;
+    }
+}
+
+// check password
+export const checkPassword = async ({ id, password }) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    try {
+        const response = await axios.post(`${API_URL}/stores/prepay/is_valid/`, {
+            "prepay_id": id,
+            "code": password,
+        }, {
             headers: {
                 Authorization: `Token ${accessToken}`,
             },
