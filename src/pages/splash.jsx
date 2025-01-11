15f7e7d57da8907e2f7c9c1c335848e7e90e1cab
Sean Kim <seahnnnnnn@gmail.com>
2025-01-11 16:44:44 +0900

    feat: splash

---
+++ src/pages/splash.jsx
@@ -0,0 +1,20 @@
+import '../../src/globals.css';
+import logo from '../assets/icons/logo.svg';
+
+const Splash = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                backgroundColor: '#0D233A',
+            }}
+        >
+            <img src={logo} alt="Logo" style={{ height: '100px' }} />
+        </div>
+    );
+};
+
+export default Splash;
