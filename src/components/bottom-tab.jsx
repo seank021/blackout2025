65909a9a8a2e0ae4d87788626cbc5bd6dc0d0337
Sean Kim <seahnnnnnn@gmail.com>
2025-01-11 15:47:40 +0900

    feat: token 없이 home, payment 진입 시 alert

--- src/components/bottom-tab.jsx
+++ src/components/bottom-tab.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 /* icons */
 import payment from '../assets/icons/payment.png';
@@ -7,48 +7,62 @@ import mypage from '../assets/icons/mypage.png';
 
 const BottomTab = () => {
     const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleNavigation = path => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (!accessToken && (path === '/' || path === '/payment')) {
+            alert('로그인이 필요합니다.');
+            navigate('/mypage');
+        } else {
+            navigate(path);
+        }
+    };
 
     return (
         <div className="bottom-tab">
-            <Link
-                to="/payment"
+            <div
+                onClick={() => handleNavigation('/payment')}
                 style={{
                     textDecoration: 'none',
                     color: location.pathname === '/payment' ? '#007BFF' : '#000',
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
+                    cursor: 'pointer',
                 }}
             >
                 <img src={payment} alt="Payment" style={{ height: '24px' }} />
                 <div>Payment</div>
-            </Link>
-            <Link
-                to="/"
+            </div>
+            <div
+                onClick={() => handleNavigation('/')}
                 style={{
                     textDecoration: 'none',
                     color: location.pathname === '/' ? '#007BFF' : '#000',
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
+                    cursor: 'pointer',
                 }}
             >
                 <img src={home} alt="Home" style={{ height: '24px' }} />
                 <div>Home</div>
-            </Link>
-            <Link
-                to="/mypage"
+            </div>
+            <div
+                onClick={() => handleNavigation('/mypage')}
                 style={{
                     textDecoration: 'none',
                     color: location.pathname === '/mypage' ? '#007BFF' : '#000',
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
+                    cursor: 'pointer',
                 }}
             >
                 <img src={mypage} alt="Mypage" style={{ height: '24px' }} />
                 <div>My Page</div>
-            </Link>
+            </div>
         </div>
     );
 };
