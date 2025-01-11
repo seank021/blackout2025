f77f7cc675a79cd7b4fd6ba28efe35dee238eb50
Sean Kim <seahnnnnnn@gmail.com>
2025-01-12 06:13:20 +0900

    feat: code confirm

--- src/components/storeItem.jsx
+++ src/components/storeItem.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 import dropdown from '../assets/icons/dropdown.png';
 import '../styles/store.css';
+import PasswordModal from './modal';
+import { checkPassword } from '../apis/api';
 
 function StoreItem({ store, totalAmount, title }) {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedPrepayId, setSelectedPrepayId] = useState(null);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const onClickPrepay = (e, id) => {
+        e.preventDefault();
+        setSelectedPrepayId(id); // 선택한 id 저장
+        setIsModalOpen(true); // 모달 열기
+    }
+
+    const handleModalConfirm = async (id, password) => {
+        console.log('Selected prepay id:', id);
+        console.log('Entered password:', password);
+        const response = await checkPassword({ id, password });
     };
 
     return (
@@ -51,6 +67,7 @@ function StoreItem({ store, totalAmount, title }) {
                                     <li
                                         key={prepay.id}
                                         className="flex justify-between items-center border-b border-gray-200 py-2"
+                                        onClick={(e, id) => onClickPrepay(e, prepay.id)}
                                     >   
                                         <div className='flex flex-row items-center gap-1'>
                                             <span className="text-sm text-gray-700">paid by</span>
@@ -66,6 +83,7 @@ function StoreItem({ store, totalAmount, title }) {
                                     <li
                                         key={prepay.id}
                                         className="flex justify-between items-center border-b border-gray-200 py-2"
+                                        onClick={(e, id) => onClickPrepay(e, prepay.id)}
                                     >
                                         <div className='flex flex-row items-center gap-1'>
                                             <span className="text-sm text-gray-700">paid by</span>
@@ -81,6 +99,7 @@ function StoreItem({ store, totalAmount, title }) {
                                     <li
                                         key={prepay.id}
                                         className="flex justify-between items-center border-b border-gray-200 py-2"
+                                        onClick={(e, id) => onClickPrepay(e, prepay.id)}
                                     >
                                         <div className='flex flex-row items-center gap-1'>
                                             <span className="text-sm text-gray-700">paid by</span>
@@ -95,6 +114,14 @@ function StoreItem({ store, totalAmount, title }) {
                         )
                 }    
             </div>
+
+            {/* Password Modal */}
+            <PasswordModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onConfirm={handleModalConfirm} // 모달 확인 버튼 클릭 시
+                prepayId={selectedPrepayId} // 선택한 prepayId 전달
+            />
         </div>
     );
 }
