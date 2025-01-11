55e52f6a0dad9f680664911ddd040c7b3bf834de
Sean Kim <seahnnnnnn@gmail.com>
2025-01-11 15:31:39 +0900

    style: map

--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -2,30 +2,30 @@ import { useEffect } from "react";
 import "../../../src/globals.css";
 
 const Payment = () => {
-  useEffect(() => {
-    // 카카오맵 그리기
-    printKakaomap();
-  }, []);
+    useEffect(() => {
+        // 카카오맵 그리기
+        printKakaomap();
+    }, []);
 
-  function printKakaomap() {
-    if (window.kakao) {
-      var container = document.getElementById("map");
-      var options = {
-        center: new window.kakao.maps.LatLng(
-          37.365264512305174,
-          127.10676860117488
-        ),
-        level: 3,
-      };
-      var map = new window.kakao.maps.Map(container, options);
+    function printKakaomap() {
+        if (window.kakao) {
+            var container = document.getElementById("map");
+            var options = {
+                center: new window.kakao.maps.LatLng(
+                37.365264512305174,
+                127.10676860117488
+                ),
+                level: 3,
+            };
+            var map = new window.kakao.maps.Map(container, options);
+        }
     }
-  }
-  return (
-    <div className="container-col p-4">
-      <h1>Payment</h1>
-      <div id="map" style={{ width: "500px", height: "400px" }}></div>
-    </div>
-  );
+
+    return (
+        <div className="container-col">
+            <div id="map" style={{ height: "400px" }}></div>
+        </div>
+    );
 };
 
 export default Payment;
